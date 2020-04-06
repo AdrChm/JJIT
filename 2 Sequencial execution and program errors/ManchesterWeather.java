403d4a65ab2e7b4e93089569b572b8e82d48885b
Adrian <adrianchmielewski11@gmail.com>
2020-04-06 09:57:42 +0200

    2.4.2 ManchesterWeather

---
+++ 2 Sequencial execution and program errors/ManchesterWeather.java
@@ -0,0 +1,13 @@
+public class ManchesterWeather
+{
+// 1)	public static avoid main(spring[] args)
+// 2)	public static void main(spring[] args)
+public static void main(String[] args)
+	{
+// 3)		Cistern.flush.printline("Rainfalls keep dropping on my head!");
+// 4)		System.flush.printline("Rainfalls keep dropping on my head!");
+// 5)		System.out.printline("Rainfalls keep dropping on my head!");
+// 6)		System.out.printline("Rainfalls keep dropping on my head!");
+			System.out.println("Rainfalls keep dropping on my head!");
+	}
+}
