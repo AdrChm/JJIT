4f143548e5c1f5bdd74b3f2f75dc966bbbc2fa37
Adrian <adrianchmielewski11@gmail.com>
2020-04-06 09:41:23 +0200

    2.3.2 Fortune

---
+++ 2 Sequencial execution and program errors/Fortune.java
@@ -0,0 +1,25 @@
+/*
+public class Fortune
+{
+// 1)	public static void main(String[] args] - one error
+	public static void main(String[] args)
+	{
+// 2) 		System.out,println('Sometimes having a fortune is too expensive!"); - two errors 
+// 3) 		System.out.println('Sometimes having a fortune is too expensive!"); - seven deadly errors
+		System.out.println("Sometimes having a fortune is too expensive!");
+	}
+}
+*/
+
+//Special
+
+public class Fortune
+{
+	publicstatic void main(String[] args] // 1 + 1
+	{ 
+{	//+1
+		System.out,println('Sometimes having a fortune is too expensive!"); 2 and 7
+
+	}
+	//total 12
+}
