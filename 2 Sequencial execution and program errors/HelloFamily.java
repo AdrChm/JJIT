56da3f9417b066c3a8825a660884baea1e4be801
Adrian <adrianchmielewski11@gmail.com>
2020-04-06 11:02:03 +0200

    2.5.2 HelloFamily

---
+++ 2 Sequencial execution and program errors/HelloFamily.java
@@ -0,0 +1,22 @@
+public class HelloFamily
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Adrian");
+		System.out.println("Hello Agata");
+		System.out.println("Hello Dorota");	
+		System.out.println("Hello Grzegorz");
+		System.out.println("Hello Ilona");
+		System.out.println("Hello Jacek");
+		System.out.println("Hello Kamil");
+		System.out.println("Hello Lucyna");
+		System.out.println("Hello Małgosia");
+		System.out.println("Hello Maus");
+		System.out.println("Hello Milena");
+		System.out.println("Hello Nikola");
+		System.out.println("Hello Paulina");
+		System.out.println("Hello Paweł");	
+		System.out.println("Hello Tomek");
+	
+	}
+}
