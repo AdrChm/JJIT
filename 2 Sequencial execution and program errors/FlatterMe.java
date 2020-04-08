224b72e7524d5f5c7bc18914042d27fc0eaefb2f
Adrian <adrianchmielewski11@gmail.com>
2020-04-08 21:47:31 +0200

    2.7.2 FlatterMe

---
+++ 2 Sequencial execution and program errors/FlatterMe.java
@@ -0,0 +1,10 @@
+public class FlatterMe
+{
+	public static void main(String[] args)
+	{
+		System.out.println(args[0] + ", you did well");
+		System.out.println("Very nice " + args[0]);
+		System.out.println("Good job " + args[0] + ", keep going!");
+	}
+	
+}
