509f3dfb4b1f0606cf27cd27bfe4b03809c67cc1
Adrian <adrianchmielewski11@gmail.com>
2020-04-08 21:46:23 +0200

    2.6.2 Quote

---
+++ 2 Sequencial execution and program errors/Quote.java
@@ -0,0 +1,14 @@
+public class Quote
+{
+// 1)	public void Main(String args)
+// 2)	public void main(String args)
+// 3)	public void main(String[] args)
+		public static void main(String[] pancz)
+	{
+		System.out.println("Programming is about making the stupid seem clever");
+		System.out.println("                       - ^ -                      ");
+		System.out.println("                       0 / 0                      ");
+		System.out.println("                         =                        ");
+		System.out.println("           (At least, to the dubm user!)          ");		
+	}
+}
