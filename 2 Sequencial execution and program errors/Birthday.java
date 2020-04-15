755cfd567c6425dc65d238777d8ab696c33fe5c9
Adrian <adrianchmielewski11@gmail.com>
2020-04-16 00:23:00 +0200

    2.8.2 Birthday

--- 2 Sequencial execution and program errors/Birthday.java
+++ 2 Sequencial execution and program errors/Birthday.java
@@ -2,7 +2,9 @@ public class Birthday
 {
 	public static void main(String [] args)
 	{
-		System.out.print("Name: + args[1] + , " + args[2] + ";");
-		System.out.println("Born: " + args[0]);
+//		System.out.print("Name: + args[1] + , " + args[2] + ";");
+//		System.out.println("Born: " + args[0]);
+		System.out.print("Name: " + args[0] + ", "  + args[1] + "; ");
+		System.out.println("Born: " + args[2]);
 	}
 }
