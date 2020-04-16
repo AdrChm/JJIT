eb668915f0687c3e85272981034a4000d80de915
Adrian <adrianchmielewski11@gmail.com>
2020-04-16 19:43:34 +0200

    3.6.1 SumFirstN

---
+++ 3 Types, variables and expressions/SumFirstN.java
@@ -0,0 +1,13 @@
+public class SumFirstN
+{
+	public static void main(String[] args)
+	{
+		int n = Integer.parseInt(args[0]);
+		int sumOfFirstN = (1 + n) / 2 * n;
+		System.out.println("The sum of first " + n + " number is " + sumOfFirstN);
+
+		//coffee 3.6.2
+		int sumOfFirstN2 = (1 + n) / 2 * n + n/2; 
+		System.out.println("The sum of first " + n + " number is " + sumOfFirstN2);		
+	}
+}
