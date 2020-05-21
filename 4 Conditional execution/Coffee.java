fd2f92e1a1ea29dfdaf719b95a6a84fd0f9469e5
Adrian <adrianchmielewski11@gmail.com>
2020-05-21 14:32:42 +0200

    3.4 Coffee

--- 4 Conditional execution/Coffee.java
+++ 4 Conditional execution/Coffee.java
@@ -18,6 +18,22 @@ public class Coffee
         else if (y < z)
         	System.out.println("y is less than z");  
         else
-        	System.out.println("I belive in  Father Christmas!");               	
+        	System.out.println("I belive in  Father Christmas!");  
+
+        //4.4.1 
+
+        int noOfPeopleInviteToTheStreetParty = Integer.parseInt(args[0]);
+
+        if(noOfPeopleInviteToTheStreetParty > 500);
+            System.out.println("You many need an entertaiment license!");   
+
+
+        //4.4.2 
+
+        if(noOfPeopleInviteToTheStreetParty > 100)
+            if(noOfPeopleInviteToTheStreetParty > 500)
+                System.out.println("You many need an entertaiment license!");  
+            else   
+                System.out.println("It will be a fairly quiet party.");      	
     }
 }
