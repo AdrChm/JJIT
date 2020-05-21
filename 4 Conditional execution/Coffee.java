f5034c06c0d0c91d07149c212dfcc00c1b5832ab
Adrian <adrianchmielewski11@gmail.com>
2020-05-21 13:12:49 +0200

    4.2.3 Coffee

---
+++ 4 Conditional execution/Coffee.java
@@ -0,0 +1,23 @@
+public class Coffee
+{
+    public static void main(String[] args) {
+
+        //4.2.1
+
+        System.out.println(5>4 == 9 < 15);
+        System.out.println(5<4 != 9 > 15);
+
+        //4.2.3
+
+        int x = 1;
+        int y = 2;
+        int z = 3;
+
+        if(x < y)
+        	System.out.println("x is less than y");
+        else if (y < z)
+        	System.out.println("y is less than z");  
+        else
+        	System.out.println("I belive in  Father Christmas!");               	
+    }
+}
