f6af0116eeae1980f7d4d4587774aff388a9108c
Adrian <adrianchmielewski11@gmail.com>
2020-05-24 15:09:33 +0200

    5.2 MinimumTankSize

---
+++ 5 Repeated execution/MinimumTankSize.java
@@ -0,0 +1,15 @@
+public class MinimumTankSize
+{
+	public static void main (String [] args)
+	{
+		double requiredVolume = Double.parseDouble(args[0]);
+		double sideLength = 0.5;
+
+		while(sideLength * sideLength * sideLength < requiredVolume)
+			sideLength = sideLength + 0.5;
+
+		System.out.println("You need a tank of " + sideLength
+							+ " meters per side to hold the volume "
+							+ requiredVolume + " cubic meters");
+	}
+}
