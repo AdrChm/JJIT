cbc3e76fa640543c42d32039c6f6742cf218ca4f
Adrian <adrianchmielewski11@gmail.com>
2020-05-24 16:44:22 +0200

    5.6 CompoundInterestKnownYears

---
+++ 5 Repeated execution/CompoundInterestKnownYears.java
@@ -0,0 +1,18 @@
+public class CompoundInterestKnownYears
+{
+	public static void main(String [] args)
+	{
+		double initialInvestment = Double.parseDouble(args[0]);
+		double interestRate = Double.parseDouble(args[1]); 
+		int noOfYearsInvested = Integer.parseInt(args[2]); 
+		double currentBalance = initialInvestment;
+
+		for (int year = 1; year <= noOfYearsInvested; year = year + 1)		
+			currentBalance = currentBalance + currentBalance * interestRate/100;
+
+		System.out.println(initialInvestment + " invested at interest rate "
+							+ interestRate + "%");
+		System.out.println("After " + noOfYearsInvestedSoFar + " years,"
+							+ " the balance will be " + currentBalance);
+	}
+}
