c6f5d8167531e3f1cd7b3049ac9af7711d35dd01
Adrian <adrianchmielewski11@gmail.com>
2020-05-24 16:17:36 +0200

    5.5.2 MinimumBitWidth

--- 5 Repeated execution/MinimumBitWidth.java
+++ 5 Repeated execution/MinimumBitWidth.java
@@ -2,13 +2,43 @@ public class MinimumBitWidth
 {
 	public static void main(String [] args)
 	{
+		//fix 1
+
 		int numberOfValues = Integer.parseInt(args[0]);
+		double numberOfValues2 = Double.parseDouble(args[0]);
 		int noOfBits = 0;
 
-		while(Math.pow(2,noOfBits) < numberOfValues)
+		int speedUpVariable = 1;
+
+		while(speedUpVariable < numberOfValues2/2)
+		//while(speedUpVariable < numberOfValues)
+		{
+			speedUpVariable = speedUpVariable * 2;
 			noOfBits = noOfBits + 1;
+
+		}
+		noOfBits = noOfBits + 1;
+
 
 		System.out.println("You need " + noOfBits + " bits to represent "
 							+ numberOfValues + " values");
+
+		// fix 2
+
+		int numberOfValues3 = Integer.parseInt(args[0]);
+		int noOfBits2 = 0;
+		long speedUpVariable2 = 1;
+
+		while(speedUpVariable2 < numberOfValues3)
+		//while(speedUpVariable < numberOfValues)
+		{
+			speedUpVariable2 = speedUpVariable2 * 2;
+			noOfBits2 = noOfBits2 + 1;
+
+		}
+
+
+		System.out.println("You need " + noOfBits2 + " bits to represent "
+							+ numberOfValues3 + " values");
 	}
 }
