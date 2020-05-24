7aa9d43181d19e9312d9a45878aec14ffc25a782
Adrian <adrianchmielewski11@gmail.com>
2020-05-24 15:19:13 +0200

    5.5 CompoundInterestKnownTarget

---
+++ 5 Repeated execution/CompoundInterestKnownTarget.java
@@ -0,0 +1,21 @@
+public class CompoundInterestKnownTarget
+{
+	public static void main(String [] args)
+	{
+		double initialInvestment = Double.parseDouble(args[0]);
+		double targetInvest = Double.parseDouble(args[1]);
+		double interestRate = Double.parseDouble(args[2]); 
+		int noOfYearsInvestedSoFar = 0;
+		double currentBalance = initialInvestment;
+
+		while( currentBalance < targetInvest)
+		{
+			noOfYearsInvestedSoFar = noOfYearsInvestedSoFar + 1;
+			currentBalance = currentBalance + currentBalance * interestRate/100;
+		}
+		System.out.println(initialInvestment + " invested at interest rate "
+							+ interestRate + "%");
+		System.out.println("After " + noOfYearsInvestedSoFar + " years,"
+							+ " the balance will be " + currentBalance);
+	}
+}
