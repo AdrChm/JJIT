83175112879dacdd039f088fdd22cbd54438c098
Adrian <adrianchmielewski11@gmail.com>
2020-05-25 20:10:37 +0200

    5.8.2 SinTable

---
+++ 5 Repeated execution/SinTable.java
@@ -0,0 +1,19 @@
+public class SinTable
+{
+	public static void main(String [] args)
+	{
+		int startDegree = Integer.parseInt(args[0]);
+		int increment = Integer.parseInt(args[1]);
+		int endDegree = Integer.parseInt(args[2]);	
+		
+		System.out.println("--------------------------------");
+		System.out.println("Sin table from " + startDegree + " to " + endDegree
+							+ " in steps of " + increment);	
+		System.out.println("--------------------------------");
+
+		for ( int degree = startDegree; degree <= endDegree; degree = degree + increment)
+			System.out.println( "| sin(" + degree + ") = " + Math.sin(Math.toRadians(degree)));
+
+		System.out.println("--------------------------------");				
+	}
+}
