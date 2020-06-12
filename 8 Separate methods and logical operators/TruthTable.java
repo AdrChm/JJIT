64e58f38bb571381add268cf965b86b6d8b67a8c
Adrian <adrianchmielewski11@gmail.com>
2020-06-12 02:59:27 +0200

    8.7.1 TruthTable

--- 8 Separate methods and logical operators/TruthTable.java
+++ 8 Separate methods and logical operators/TruthTable.java
@@ -1,40 +1,34 @@
-// Upgrade 8.7.2
 // Program prints out the truth table 
-// for two hardcoded propositional expressions p1 and p2 and p3.
-// The expression have two boolean variables, a, b, c and d.
+// for two hardcoded propositional expressions p1 and p2.
+// The expression have two boolean variables, a, b and c.
 // Each locumn of the table ocuppies 7 characters plus separator.
-public class TruthTable34
+public class TruthTable
 {
 	// First propositional expression, p1.
-	private static boolean p1(boolean a, boolean b, boolean c, boolean d)
+	private static boolean p1(boolean a, boolean b, boolean c)
 	{
-		return (((a || b) && c) || ((b || c) && d)) && (a || d);
+		return a && (b || c);
 	} // p1
 
 
 	// Second propositional expression, p2.
-	private static boolean p2(boolean a, boolean b, boolean c, boolean d)
+	private static boolean p2(boolean a, boolean b, boolean c)
 	{
-		return (a && c || b && d || c && d);
+		return a || b && a || c;
 	} // p2
 
-	// Third propositional expression, p3.
-	private static boolean p3(boolean a, boolean b, boolean c, boolean d)
-	{
-		return ((b || c) && (c || d) && (a && d));
-	} // p3
 
 	// Print a line of underscores as wide as the truth table.
 	private static void printStraightLine()
 	{
-		System.out.println(" ------------------------------------------------------- ");
+		System.out.println(" --------------------------------------- ");
 	} // printStraightLine
 
 
 	// Print the heading for the truth table.
 	private static void printHeadings()
 	{
-		System.out.println("|   a   |   b   |   c   |   d   |  p1   |  p2   |  p3   |");
+		System.out.println("|   a   |   b   |   c   |  p1   |  p2   |");
 	} // printHeadings 
 
 
@@ -42,20 +36,18 @@ public class TruthTable34
 	// with vertical bars for the column separators.
 	private static void printColumnsLine()
 	{
-
-		System.out.println("|_______|_______|_______|_______|_______|_______|_______|");		
+		System.out.println("|_______|_______|_______|_______|_______|");		
 	} // printColumnsLine
 
 
 	// To print a row, we use formatRowItem to make the 
 	// column entires have 7 characters.
-	private static void printRow(boolean a, boolean b, boolean c, boolean d)
+	private static void printRow(boolean a, boolean b, boolean c)
 	{
 		System.out.println("|" + formatRowItem(a) + "|" + formatRowItem(b)
-							 + "|" + formatRowItem(c) + "|" + formatRowItem(d) 
-							 + "|" + formatRowItem(p1(a, b, c, d))
-							 + "|" + formatRowItem(p2(a, b, c, d))
-							 + "|" + formatRowItem(p3(a, b, c, d)) + "|");		
+							 + "|" + formatRowItem(c)
+							 + "|" + formatRowItem(p1(a, b,c ))
+							 + "|" + formatRowItem(p2(a, b,c )) + "|");		
 	} // printRow	
 
 
@@ -75,7 +67,7 @@ public class TruthTable34
 		printColumnsLine();
 
 		// Start off with all three variables being ture.
-		boolean a = true, b = true, c = true, d = true;
+		boolean a = true, b = true, c = true;
 
 		// Execute twice for the 'a' variable
 		// and ensure 'a' goes from true to false.
@@ -84,12 +76,10 @@ public class TruthTable34
 			for(int bCount = 1; bCount <= 2; bCount++, b = !b)
 			// Do the same for 'c' for each 'b' value.
 				for(int cCount = 1; cCount <= 2; cCount++, c = !c)
-				// Do the same for 'c' for each 'b' value.
-					for(int dCount = 1; dCount <= 2; dCount++, d = !d)
-						// Print a row for each a, b and c combination.
-						printRow(a, b, c, d);
+			// Print a row for each a, b and c combination.
+				printRow(a, b, c);
 			
 		printColumnsLine();
 	} // main
 
-} // class TruthTable 
+} // class ThuthTable 
