16425efe83310ea1a73748c8ce671908b04187b8
Adrian <adrianchmielewski11@gmail.com>
2020-07-25 17:28:07 +0200

    9 Chapter review

---
+++ 9 Consolidation of concepts so far/ChapterReview9.java
@@ -0,0 +1,15 @@
+// This class contain answers to review questions 
+// at the end of chapter 9
+
+public class ChapterReview9{
+
+	public static void main (String args[]){
+
+	// 155.1
+		System.out.println( 6 % 3 == 0 ? "correct" : "wrong, it is false!");
+		System.out.println( 8 % 4 == 2 ? "wrong, it is true!" : "correct");	
+		System.out.println( 5 % 4 > 12 % 10 ? "wrong, it is true!" : "correct");	
+		System.out.println( 8 % 4 < 16 % 12 ? "correct" : "wrong, it is false!");
+
+	}
+}
