deb6c383b8483eeb5c26d597598352ce59528c71
AdrChm <adrianchmielewski11@gmail.com>
2022-05-15 18:04:33 +0200

    2.7.2 FlatterMe class

---
+++ FlatterMe.java
@@ -0,0 +1,9 @@
+public class FlatterMe
+{
+	public static void main (String [] args)
+	{
+		System.out.println("Hello " + args[0]);
+		System.out.println(args[0] + ", you look great!");
+		System.out.println("Hey, " + args[0] + " let's code together!");
+ 	}
+}
