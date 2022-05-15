3e3543db8fb1df981b6b51103571772caa9074f9
AdrChm <adrianchmielewski11@gmail.com>
2022-05-15 15:03:01 +0200

    2.4.2 ManchesterWeather class fix

---
+++ ManchesterWeather.java
@@ -0,0 +1,12 @@
+public class ManchesterWeather
+{
+	//before fix: 
+	//public static avoid main(spring[] args)
+	public static void main(String[] args)
+	{
+	//before fix: 
+	//Cistern.flush.printline("Rainfalls keep dropping on my head!");
+		System.out.println("Rainfalls keep dropping on my head!");
+	}
+
+}
