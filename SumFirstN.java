b0b5b297a1f2cb5f02a8caf9ea956d2d922d939e
AdrChm <adrianchmielewski11@gmail.com>
2022-05-17 20:01:25 +0200

    3.8.1 SumFirstN class - even numbers fix

--- SumFirstN.java
+++ SumFirstN.java
@@ -3,7 +3,7 @@ public class SumFirstN
 	public static void main(String [] args)
 	{
 		int n = Integer.parseInt(args[0]);	
-		int sumOfFirstN = (n + 1) / 2 * n;
+		int sumOfFirstN = (n + 1) * n / 2 ;
 		System.out.println("The sum of first " + n + " numbers is " + sumOfFirstN);
 	}
 }
