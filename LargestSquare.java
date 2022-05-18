7d29e8023112027c46a6a4a8c338bee301a24ebb
AdrChm <adrianchmielewski11@gmail.com>
2022-05-18 19:34:49 +0200

    5.3.2 LargestSquare - fastest route

--- LargestSquare.java
+++ LargestSquare.java
@@ -3,15 +3,10 @@ public class LargestSquare
 	public static void main(String [] args)
 	{
 		int givenNumber = Integer.parseInt(args[0]);
-		int squaredNumber = givenNumber;
-		double squareValue = Math.pow(squaredNumber,2);
-	
-		while (givenNumber < squareValue)
-		{
-			squaredNumber = squaredNumber -1;
-			squareValue = Math.pow(squaredNumber,2);
-		}
+		double rootValue = Math.sqrt(givenNumber);
+		double floorValue = Math.floor(rootValue);
+
 		System.out.println("The biggest square for " + givenNumber 
-				  + " is " + squaredNumber);
+				  + " is " + floorValue);
 	}
 }
