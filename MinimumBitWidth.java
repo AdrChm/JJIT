f8ca6ab6af4194c3519af1a67d059599c20b58c3
AdrChm <adrianchmielewski11@gmail.com>
2022-05-21 13:57:57 +0200

    5.5.2 MinimumBitWidth - calculated by doubling + fix

--- MinimumBitWidth.java
+++ MinimumBitWidth.java
@@ -10,8 +10,17 @@ public class MinimumBitWidth
 		{
 			noOfBits = noOfBits + 1;
 			doubledValue = doubledValue * 2;
-		System.out.println(noOfBits + "," + doubledValue);
+			System.out.println(noOfBits + "," + doubledValue);
+		
+			// inflinite loop fix
+			if(doubledValue < 0)
+			{
+				doubledValue = numberOfValues;
+				System.out.println("Given input exceeded maximum allowed value "
+				+ "of 1073741825, so the result displayed below is incorrect");
+			}
 		}
+		
 		System.out.println("You need " + noOfBits + " bits to represent "
 				  + numberOfValues + " values");
 	}
