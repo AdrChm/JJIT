18ecb5b01d32f30ab0ba4b7f894b26ae5abc9718
AdrChm <adrianchmielewski11@gmail.com>
2022-05-22 14:45:32 +0200

    6.6 TimesTable class - new program

--- TimesTable.java
+++ TimesTable.java
@@ -1,15 +1,60 @@
+// Program to print out a neat 10 by 10 multiplication table.
 public class TimesTable
 {
 	public static void main(String [] args)
 	{
-		int multiplier = Integer.parseInt(args[0]);
+		// Top line.
+		// Left side, 5 characters for row labels, separator.
+		System.out.print("|-----|");
+		// Above the column headings.
+		for (int column = 1; column <= 10; column++)
+			// 4 character for each column.
+			System.out.print("----");
+		// The right side.
+		System.out.println("-|");
 
-		System.out.println("--------------------------------");
-		System.out.println("| Times table for " +  multiplier);
-		System.out.println("--------------------------------");
-		for (int thisNumber = 1; thisNumber <= 10; thisNumber = thisNumber + 1)
-			System.out.println("| " + thisNumber + " x " + multiplier
-					  + " = " + thisNumber * multiplier);
-		System.out.println("--------------------------------");
-	}
-}
+		// Column headings.
+		System.out.print("|     |");
+		for (int column = 1; column <= 10; column++)
+			// Need to make column number always occupy 4 characters.
+			if (column < 10)	
+				System.out.print("   " + column);					
+			else 
+				System.out.print("  " + column);
+			System.out.println(" |");
+
+		// Now the rows.
+		for (int row = 1; row <= 10; row++)	
+		{
+			// Need to make row nubmer always occupy 7 characters
+			// including vertical lines.
+			if(row < 10)
+				System.out.print("|   " + row + " |");
+			else 
+				System.out.print("|  " + row + " |");					
+			
+			// Now the column on this row.
+			for (int column = 1; column <= 10; column++)	
+			{
+				int product = row * column;
+				// Need to make product always occupy 4 characters.
+				if (product < 10)
+					System.out.print("   " + product);
+				else if (product < 100)
+					System.out.print("  " + product);
+				else
+					System.out.print(" " + product);		
+			} // for
+			
+			// The right side.
+			System.out.println(" |");
+		} // for
+		
+		// Bottom line -- same as Top line.
+		System.out.print("|-----|");
+		for (int column = 1; column <= 10; column++)
+			System.out.print("----");
+		System.out.println("-|");	
+					
+	} // main
+} // TimesTable class
