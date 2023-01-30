d83d3aaa7d2f7dd284199b150ac84580525c2228
AdrChm <adrianchmielewski11@gmail.com>
2023-01-30 15:20:43 +0100

    3.6.3 FishTankMaterials - fix, chapter review

--- FishTankMaterials.java
+++ FishTankMaterials.java
@@ -3,23 +3,29 @@ public class FishTankMaterials
 	public static void main(String [] args)
 	{
 		int width = Integer.parseInt(args[0]);
-		int depth = Integer.parseInt(args[1]);
+		int lenght = Integer.parseInt(args[1]);
 		int height = Integer.parseInt(args[2]);	
 		
 		/* before fix: 
-		int surfaceArea = width + height + depth + 2 * width + depth;
-		int edgesLength = height * width * depth + 4;	
+		int surfaceArea = width + height + lenght + 2 * width + lenght; 
+		int edgesLength = height * width * lenght + 4;	
+		*/
+		
+		/* another fix - changed as top is not required 
+		I would also renamed "depth" into "lenght", as it's more clear.
+		Class requires further fixing, but at this point more shouldn't be done 
+		int surfaceArea = (width * height + width * lenght + height * lenght) * 2;
 		*/
 
-		int surfaceArea = (width * height + width * depth + height * depth) * 2;
-		int edgesLength = (height + width + depth) * 4;	
+		int surfaceArea = width * height * 2 + height * lenght * 2 + width * lenght;
+		int edgesLength = (height + width + lenght) * 4;	
 
 		System.out.println("The surface area of a tank with dimentions "
-				  + "(" + width + "," + depth + "," + height + ") "
+				  + "(" + width + "," + lenght + "," + height + ") "
 				  + "is " + surfaceArea);
 			
 		System.out.println("The lenght of the edges of a tank with dimentions "
-				  + "(" + width + "," + depth + "," + height + ") "
+				  + "(" + width + "," + lenght + "," + height + ") "
 				  + "is " + edgesLength);
 		
 	}
