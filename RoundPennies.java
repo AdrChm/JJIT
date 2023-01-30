3821b60b0d7bc4b0ab6e3f51ad26913a164d72d9
AdrChm <adrianchmielewski11@gmail.com>
2023-01-30 16:14:17 +0100

    3.8.2 RoundPennies - chapter review

--- RoundPennies.java
+++ RoundPennies.java
@@ -5,8 +5,11 @@ public class RoundPennies
 		int pennies = Integer.parseInt(args[0]);
 		int rawPounds = (pennies)/100;
 		int theRest = pennies - rawPounds*100;
+
+		/* chapter review (before)
 		int theRestRound = theRest*2/100;
-		
+		*/
+		int theRestRound = theRest/50;
 		System.out.println("You have "+ (rawPounds + theRestRound) + " pounds");
 		 
 		
