d0699b11b1cf09ada9c439f70bfd93add98daede
AdrChm <adrianchmielewski11@gmail.com>
2023-03-09 00:24:22 +0100

    6.6.1 TimesTable - Coffee 6.6.1 update

--- TimesTable.java
+++ TimesTable.java
@@ -1,13 +1,17 @@
-// Program to print out a neat 10 by 10 multiplication table.
+// Program to print out a neat x by x multiplication table, where x is the size of the table
+// Which is given as input argument
 public class TimesTable
 {
 	public static void main(String [] args)
 	{
+		// Coffee 6.6.1 update - provide input for size flexibility
+		int size = Integer.parseInt(args[0]);		
+
 		// Top line.
 		// Left side, 5 characters for row labels, separator.
 		System.out.print("|-----|");
 		// Above the column headings.
-		for (int column = 1; column <= 10; column++)
+		for (int column = 1; column <= size; column++)
 			// 4 character for each column.
 			System.out.print("----");
 		// The right side.
@@ -15,7 +19,7 @@ public class TimesTable
 
 		// Column headings.
 		System.out.print("|     |");
-		for (int column = 1; column <= 10; column++)
+		for (int column = 1; column <= size; column++)
 			// Need to make column number always occupy 4 characters.
 			if (column < 10)	
 				System.out.print("   " + column);					
@@ -24,7 +28,7 @@ public class TimesTable
 			System.out.println(" |");
 
 		// Now the rows.
-		for (int row = 1; row <= 10; row++)	
+		for (int row = 1; row <= size; row++)	
 		{
 			// Need to make row nubmer always occupy 7 characters
 			// including vertical lines.
@@ -34,7 +38,7 @@ public class TimesTable
 				System.out.print("|  " + row + " |");					
 			
 			// Now the column on this row.
-			for (int column = 1; column <= 10; column++)	
+			for (int column = 1; column <= size; column++)	
 			{
 				int product = row * column;
 				// Need to make product always occupy 4 characters.
@@ -52,7 +56,7 @@ public class TimesTable
 		
 		// Bottom line -- same as Top line.
 		System.out.print("|-----|");
-		for (int column = 1; column <= 10; column++)
+		for (int column = 1; column <= size; column++)
 			System.out.print("----");
 		System.out.println("-|");	
 					
