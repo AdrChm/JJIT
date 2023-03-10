65cd21df945299d2863accfcb2a371d03511ccdd
AdrChm <adrianchmielewski11@gmail.com>
2023-03-10 16:36:20 +0100

    8.5.1 TimesTable - separate methods

--- TimesTable.java
+++ TimesTable.java
@@ -2,32 +2,102 @@
 // Which is given as input argument
 public class TimesTable
 {
+	// The size of the table -- the number of rows and columns.
+	private static int tableSize = 12;
+	
+	// The main method implements the top structure of the table.
 	public static void main(String [] args)
 	{
-		// Coffee 6.6.1 update - provide input for size flexibility
-		int size = Integer.parseInt(args[0]);		
-
+	
 		// Top line.
+		printLine();
+		
+		// Column headings.
+		printColumnHeadings();
+
+		// UnderlineHeadings
+		printLine();
+
+		// Now the rows.
+		for(int row =1; row <= tableSize; row++)
+			printRow(row);
+
+		// Bottom line.
+		printLine();	
+
+	} // main
+
+	// Print line across the table.
+	private static void printLine()
+	{
 		// Left side, 5 characters for row labels, separator.
 		System.out.print("|-----|");
-		// Above the column headings.
-		for (int column = 1; column <= size; column++)
+
+		// Across each column.
+		for (int column = 1; column <= tableSize; column++)
 			// 4 character for each column.
 			System.out.print("----");
+
 		// The right side.
 		System.out.println("-|");
 
-		// Column headings.
+	} // printLine
+
+	// Print the line containing the column headings.
+	private static void printColumnHeadings()
+	{
 		System.out.print("|     |");
-		for (int column = 1; column <= size; column++)
+		for (int column = 1; column <= tableSize; column++)
+			printNumber(column);
+			System.out.println(" |");		
+		
+	} // printColumnHeadings
+
+	// Print one row of the table.
+	private static void printRow(int row)
+	{
+		// The left side.
+			System.out.print("|");	
+		printNumber(row);	
+		// Separator
+		System.out.print(" |");	
+
+		// Now for the columns on this row.
+		for (int column = 1; column <= tableSize; column++)
+			printNumber(row * column);
+		
+		// The right side.
+		System.out.println(" |");			
+	
+	} // printRow
+
+
+	private static void printNumber(int numberToPoint)
+	{
+		if (numberToPoint < 10)
+			System.out.print("   " + numberToPoint);
+		else if (numberToPoint < 100)
+			System.out.print("  " + numberToPoint);
+		else
+			System.out.print(" " + numberToPoint);		
+	
+	} // printNumber
+
+
+
+
+
+/*		 
+		// Coffee 6.6.1 update - provide input for size flexibility
+		int size = Integer.parseInt(args[0]);	
+
 			// Need to make column number always occupy 4 characters.
 			if (column < 10)	
 				System.out.print("   " + column);					
 			else 
 				System.out.print("  " + column);
-			System.out.println(" |");
 
-		// Now the rows.
+
 		for (int row = 1; row <= size; row++)	
 		{
 			// Need to make row nubmer always occupy 7 characters
@@ -54,11 +124,6 @@ public class TimesTable
 			System.out.println(" |");
 		} // for
 		
-		// Bottom line -- same as Top line.
-		System.out.print("|-----|");
-		for (int column = 1; column <= size; column++)
-			System.out.print("----");
-		System.out.println("-|");	
-					
-	} // main
+*/					
+
 } // TimesTable class
