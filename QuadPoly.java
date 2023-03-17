8b082e768d9b208ddd56bd26455996eebdba8d65
AdrChm <adrianchmielewski11@gmail.com>
2023-03-17 16:32:11 +0100

    10.2.5 AddQuadPoly, QuadPoly - extra option

--- QuadPoly.java
+++ QuadPoly.java
@@ -1,9 +1,10 @@
 // Representation of a quadratic polynomial.
+// Each not provided coefficient has assigned value of 0.  
 public class QuadPoly
 {
-	public double coefficientOfx2;
-	public double coefficientOfx;
-	public double constant;
+	public double coefficientOfx2 = 0;
+	public double coefficientOfx = 0;
+	public double constant = 0;
 
 	// Construct a quadratic polynomial by providing its coeffients.
 	public QuadPoly(double a, double b, double c)
@@ -11,6 +12,25 @@ public class QuadPoly
 		coefficientOfx2 = a;
 		coefficientOfx = b;
 		constant = c;
+	} // QuadPoly
+
+	// 10.2.5 Option extra - additional constructors
+	// Construct a quadratic polynomial by providing its two first coeffients.
+	public QuadPoly(double a, double b)
+	{
+		coefficientOfx2 = a;
+		coefficientOfx = b;
+	} // QuadPoly
+
+	// Construct a quadratic polynomial by providing its first coeffient.
+	public QuadPoly(double a)
+	{
+		coefficientOfx2 = a;
+	} // QuadPoly
+
+	// Construct a quadratic polynomial with all coeffients being 0.
+	public QuadPoly()
+	{
 	} // QuadPoly
 
 } // class QuadPoly 
