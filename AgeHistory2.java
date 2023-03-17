9d09714198b7f4afe6c6a097dd4d7e2d06a194a0
AdrChm <adrianchmielewski11@gmail.com>
2023-03-17 14:09:34 +0100

    10.4.2 AgeHistory2, Date

--- AgeHistory2.java
+++ AgeHistory2.java
@@ -3,28 +3,31 @@
 // Each date is three numbers: day month year.
 public class AgeHistory2
 {
-	// The present date stored as 3 variables
-	private static int presentDay, presentMonth, presentYear;
+	// The present date.
+	private static Date presentDate;
 
 	// Prints the age history of one person, identified as personNumber.
 	// The birth date must be less than present date.	
-	private static void printAgeHistory(int personNumber, int birthDay, 
-					    int birthMonth, int birthYear)
+	private static void printAgeHistory(int personNumber, Date birthDate)
 	{
 		// Start by printing the event of birth.	
 		System.out.println("Pn " + personNumber + " was born on " 
-				  + birthDay + "/" +  birthMonth + "/" + birthYear);
+				  + birthDate.day + "/" +  birthDate.month
+				  + "/" + birthDate.year);
 
 		// Now we will go through the years since birth, but before today.
-		int someYear = birthYear + 1;
+		int someYear = birthDate.year + 1;
 		int ageInSomeYear = 1;
-		while (someYear < presentYear
-			|| someYear == presentYear && birthMonth < presentMonth
-			|| someYear == presentYear && birthMonth == presentMonth
-			&& birthDay < presentDay)
+		while (someYear < presentDate.year
+			|| someYear == presentDate.year 
+				&& birthDate.month < presentDate.month
+			|| someYear == presentDate.year 
+				&& birthDate.month == presentDate.month
+				&& birthDate.day < presentDate.day)
 		{
 			System.out.println("Pn " + personNumber + " was " + ageInSomeYear  
-					  + " on " + birthDay + "/" +  birthMonth + "/" + someYear);
+					  + " on " + birthDate.day + "/" +  birthDate.month
+					  + "/" + someYear);
 			someYear++; 
 			ageInSomeYear++;
 		} // while		
@@ -36,35 +39,36 @@ public class AgeHistory2
 		// someYear equals a presentYear,
 		// otherwise someYear equals presentYear + 1.
 
-		if (birthMonth == presentMonth && birthDay == presentDay) 
+		if (birthDate.month == presentDate.month && birthDate.day == presentDate.day) 
 		// then the someYear must be equal to presentYear.
 			System.out.println("Pn " + personNumber + " is" + ageInSomeYear + " today!");
 		else
-			System.out.println("Pn " + personNumber + " will be " + ageInSomeYear  
-					  + " on " + birthDay + "/" +  birthMonth + "/" + someYear);
+			System.out.println("Pn " + personNumber + " will be "
+					  + ageInSomeYear + " on " + birthDate.day + "/"
+					  +  birthDate.month + "/" + someYear);
 			
 	} // printAgeHistory
 	
 	// The main method: get arguments and call printAgeHistory. 
 	public static void main(String [] args)
 	{
-		// The present date, stored in three class variables.
-		presentDay = Integer.parseInt(args[0]);
-		presentMonth = Integer.parseInt(args[1]);
-		presentYear = Integer.parseInt(args[2]);
+		// The present date.
+		presentDate = new Date (Integer.parseInt(args[0]),
+					     Integer.parseInt(args[1]),
+					     Integer.parseInt(args[2]));
 
 		// The dates of birth: these must be less than the present date. 	
-		int birthDay1 = Integer.parseInt(args[3]);
-		int birthMonth1 = Integer.parseInt(args[4]);
-		int birthtYear1 = Integer.parseInt(args[5]);
+		Date birthDate1 = new Date (Integer.parseInt(args[3]),
+					    Integer.parseInt(args[4]),
+					    Integer.parseInt(args[5]));
 
-		int birthDay2 = Integer.parseInt(args[6]);
-		int birthMonth2 = Integer.parseInt(args[7]);
-		int birthtYear2 = Integer.parseInt(args[8]);
+		Date birthDate2 = new Date (Integer.parseInt(args[6]),
+					    Integer.parseInt(args[7]),
+					    Integer.parseInt(args[8]));
 
 		// Now print two age histories.
-		printAgeHistory(1, birthDay1, birthMonth1, birthtYear1);
-		printAgeHistory(2, birthDay2, birthMonth2, birthtYear2);
+		printAgeHistory(1, birthDate1);
+		printAgeHistory(2, birthDate2);
 	} // main
 
 } // class AgeHistory2
---
+++ Date.java
@@ -0,0 +1,15 @@
+// Representation of a date.
+public class Date
+{
+	// The day, month and year of the date.
+	public int day, month, year;
+
+	// Construct a date -- given the required day, month and year.
+	public Date(int RequiredDay, int RequiredMonth, int RequiredYear)
+	{
+		day = RequiredDay;
+		month = RequiredMonth;
+		year = RequiredYear;
+	} // Date
+
+} // class Date
