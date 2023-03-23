45c5035fd77e128baf01daa5d374794d0dbc08e7
AdrChm <adrianchmielewski11@gmail.com>
2023-03-23 20:04:25 +0100

    12.2.2 StudentsCalling, Student, Account, Phone - documentation

--- Student.java
+++ Student.java
@@ -1,25 +1,47 @@
-// Representation of the student.
+/**
+ * Representation of the student.
+ *
+ * @author Adrian Chmielewski
+ */
 public class Student
 {
-	// Not changable name.
+	// Not changeable name.
 	private final String name;
 
 	// Mobile phone owned by the student.
 	private Phone phone = null;
-	
-	// Student constructor
+
+	/**
+	 * Constructs a student.
+	 *
+	 * @param name The required, full name of the student.
+	 */
 	public Student(String name)
 	{
 		this.name = name;
 	} // Student
-	
-	// Aquiring phone 
+
+	/**
+	 * Acquisition of a new phone by the student.
+	 * If student already has a phone, old one will be discarded.
+	 *
+	 * @param model Required name of the phone model.
+	 * @param serviceProvider Required name of the service provider.
+	 * Used to created account assigned to the phone.
+	 */
 	public void getNewPhone(String model, String serviceProvider)	
 	{
 		this.phone = new Phone(model, serviceProvider);
 	} // getNewPhone;
 
-	// Make a call for given duration (in seconds)
+	/**
+	 * Student takes action to make a call for given duration (in seconds)
+	 * Action is used to make a requestCall by phone, the student is owning.
+	 * @see Phone#requestCall(int)
+	 * If student does not have a phone, nothing will happen.
+	 *
+	 * @param requiredDuration Required length of the call (in second).
+	 */
 	public void makeACall(int requiredDuration)
 	{
 		// Case when person has no phone
@@ -29,8 +51,15 @@ public class Student
 		phone.requestCall(requiredDuration);
 
 	} // makeACall
-	
-	// Tops up the phone with given amount of pounds.
+
+	/**
+	 * Student takes action to pop up the phone owned by this student by given amount of pounds.
+	 * Action is used to make a topUp by phone, the student is owning.
+	 * @see Phone#topUp(int)
+	 * If student does not have a phone, nothing will happen.
+	 *
+	 * @param pounds Required amount to top up the phone (in pounds).
+	 */
 	public void topUp(int pounds)
 	{
 		// Case when person has no phone
@@ -40,12 +69,21 @@ public class Student
 
 	} // topUp
 
-	// Returns student phone. 
+	/**
+	 * Returns phone owned by the student.
+	 *
+	 * @return Phone owned by the student.
+	 */
 	public Phone getPhone()
 	{
 		return phone;
 	} // getPhone
 
+	/**
+	 * Provides the Student( full name, phone) representation of this student.
+	 *
+	 * @return String representing this student.
+	 */
 	public String toString()
 	{
 		return "Student( " + name + "," + phone + ")";
