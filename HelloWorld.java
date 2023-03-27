e76c60539991146d1be02b00d64cee24d794bdd8
AdrChm <adrianchmielewski11@gmail.com>
2023-03-27 13:56:54 +0200

    13.2.1 HelloWorld GUI class

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,24 @@
-public class HelloWorld
-{
-	public static void main(String [] args){
-		
-		System.out.println("Bonjour le monde!");
-	}
-}
+package Chapter13;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+// Program to display hello world greeting in a window.
+public class HelloWorld extends JFrame{
+
+    // Constructor
+    public HelloWorld()
+    {
+        setTitle("Hello World");
+        Container contents = getContentPane();
+        contents.add(new JLabel("Greetings to all who dwell on Planet Earth!"));
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+    } // HelloWorld
+
+    // Create a hello world and make it appear on screen.
+    public static void main(String[] args) {
+        HelloWorld theHelloWorld = new HelloWorld();
+        theHelloWorld.setVisible(true);
+    } // main
+
+} // class HelloWorld
