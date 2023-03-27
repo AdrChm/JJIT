6ac1cb73b3b8040f0f54cada0d3ed5ef9b46e789
AdrChm <adrianchmielewski11@gmail.com>
2023-03-27 16:03:40 +0200

    13.5.1 TimesTable GUI

--- TimesTable.java
+++ TimesTable.java
@@ -1,84 +1,39 @@
-// Program to print out a neat x by x multiplication table, where x is the size of the table
-// Which is given as input argument
-public class TimesTable
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+// Program to print out a neat multiplication table.
+// It can take up to two arguments:
+// First describes scope of the table (starting from one)
+// Second describes number, which will be multiplied by each table element.
+public class TimesTable extends JFrame
 {
-	// The size of the table -- the number of rows and columns.
-	private static int tableSize = 12;
-	
-	// The main method implements the top structure of the table.
-	public static void main(String [] args)
-	{
-	
-		// Top line.
-		printLine();
-		
-		// Column headings.
-		printColumnHeadings();
+	// Constructor method
+	public TimesTable(int tableSize, int numberToMultiply) {
 
-		// UnderlineHeadings
-		printLine();
+		setTitle( tableSize + " x " + numberToMultiply);
+		Container content = getContentPane();
+		content.setLayout(new GridLayout(0,5, 50, 5));
 
-		// Now the rows.
-		for(int row =1; row <= tableSize; row++)
-			printRow(row);
+		for (int index = 1; index <= tableSize; index++) {
+			content.add(new JLabel("" + index));
+			content.add(new JLabel("X"));
+			content.add(new JLabel("" + numberToMultiply));
+			content.add(new JLabel("="));
+			content.add(new JLabel("" + index * numberToMultiply));
+		}
 
-		// Bottom line.
-		printLine();	
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+	} // TimesTable
 
-	} // main
+	// Create a TimesTable and make it appear on screen.
+	public static void main(String[] args) {
 
-	// Print line across the table.
-	private static void printLine()
-	{
-		// Left side, 5 characters for row labels, separator.
-		System.out.print("|-----|");
+		TimesTable theTimesTable = new TimesTable(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+		theTimesTable.setVisible(true);
+	}
 
-		// Across each column.
-		for (int column = 1; column <= tableSize; column++)
-			// 4 character for each column.
-			System.out.print("----");
-
-		// The right side.
-		System.out.println("-|");
-
-	} // printLine
-
-	// Print the line containing the column headings.
-	private static void printColumnHeadings()
-	{
-		System.out.print("|     |");
-		for (int column = 1; column <= tableSize; column++)
-			printNumber(column);
-			System.out.println(" |");		
-		
-	} // printColumnHeadings
-
-	// Print one row of the table.
-	private static void printRow(int row)
-	{
-		// The left side.
-			System.out.print("|");	
-		printNumber(row);	
-		// Separator
-		System.out.print(" |");	
-
-		// Now for the columns on this row.
-		for (int column = 1; column <= tableSize; column++)
-			printNumber(row * column);
-		
-		// The right side.
-		System.out.println(" |");			
-	
-	} // printRow
-
-
-	private static void printNumber(int numberToPoint)
-	{
-				 
-		// Coffee 8.5.1 update - string formatting with printf
-
-			System.out.printf("%4d", numberToPoint);		
-	
-	} // printNumber
 
 } // TimesTable class
