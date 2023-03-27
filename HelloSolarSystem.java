dc81081d5d4a916d392a0697f4bb3e23e400d229
AdrChm <adrianchmielewski11@gmail.com>
2023-03-27 14:47:05 +0200

    13.3.1 HelloSolarSystem GUI class

--- HelloSolarSystem.java
+++ HelloSolarSystem.java
@@ -1,17 +1,35 @@
-public class HelloSolarSystem {
+import java.awt.FlowLayout;
+import java.awt.Container;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+// Program to display greetings to all 9 planets, in a window.
+public class HelloSolarSystem extends JFrame{
 
+	// Constructor
+	public HelloSolarSystem()
+	{
+		setTitle("Hello Solar System");
+		Container contents = getContentPane();
+		contents.setLayout( new FlowLayout());
+
+		contents.add(new JLabel("Hello Mercury!"));
+		contents.add(new JLabel("Hello Venus!"));
+		contents.add(new JLabel("Hello Earth!"));
+		contents.add(new JLabel("Hello Mars!"));
+		contents.add(new JLabel("Hello Jupiter!"));
+		contents.add(new JLabel("Hello Saturn!"));
+		contents.add(new JLabel("Hello Uranus!"));
+		contents.add(new JLabel("Hello Neptune!"));
+		contents.add(new JLabel("Goodbye Pluto!"));
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+	} // HelloSolarSystem
+
+	// Create a HelloSolarSystem and make it appear on screen.
 	public static void main(String [] args){
+		HelloSolarSystem theHelloSolarSystem = new HelloSolarSystem();
+		theHelloSolarSystem.setVisible(true);
+	} // main
 
-		System.out.println("Hello Mercury!");
-		System.out.println("Hello Venus!");
-		System.out.println("Hello Earth!");
-		System.out.println("Hello Mars!");
-		System.out.println("Hello Jupiter!");
-		System.out.println("Hello Saturn!");
-		System.out.println("Hello Uranus!");
-		System.out.println("Hello Neptune!");
-		System.out.println("Goodbye Pluto!");
-
-	}
-
-} 
+} // class HelloSolarSystem
