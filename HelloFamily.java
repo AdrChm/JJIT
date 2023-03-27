504ea27ebb1796870d60792ca3c18d4bcf9508f0
AdrChm <adrianchmielewski11@gmail.com>
2023-03-27 14:58:44 +0200

    13.3.2 HelloFamily GUI class

--- HelloFamily.java
+++ HelloFamily.java
@@ -1,19 +1,39 @@
-public class HelloFamily
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Container;
+import java.awt.FlowLayout;
+
+// Program to greet my family members.
+public class HelloFamily extends JFrame
 {
+	public HelloFamily()
+	{
+		setTitle("Hello family!");
+		Container content = getContentPane();
+		content.setLayout(new FlowLayout());
+		content.add(new JLabel("Hello Adam!"));
+		content.add(new JLabel("Hello Adrian!"));
+		content.add(new JLabel("Hello Dorota!"));
+		content.add(new JLabel("Hello Grzegorz!"));
+		content.add(new JLabel("Hello Jola!"));
+		content.add(new JLabel("Hello Lucyna!"));
+		content.add(new JLabel("Hello Marek!"));
+		content.add(new JLabel("Hello Milena!"));
+		content.add(new JLabel("Hello Paulina!"));
+		content.add(new JLabel("Hello Paweł!"));
+		content.add(new JLabel("Hello Regina!"));
+		content.add(new JLabel("Hello Tomek!"));
+
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		pack();
+
+	} // HelloFamily
+
+	// Generated HelloFamily class and make it appear on screen.
 	public static void main(String [] args)
 	{
-		System.out.println("Hello Adam!");
-		System.out.println("Hello Adrian!");
-		System.out.println("Hello Dorota!");
-		System.out.println("Hello Grzegorz!");
-		System.out.println("Hello Jola!");
-		System.out.println("Hello Lucyna!");
-		System.out.println("Hello Marek!");
-		System.out.println("Hello Milena!");
-		System.out.println("Hello Paulina!");
-		System.out.println("Hello Paweł!");
-		System.out.println("Hello Regina!");
-		System.out.println("Hello Tomek!");
+		HelloFamily theHelloFamily = new HelloFamily();
+		theHelloFamily.setVisible(true);
+	} // main
 
-	}
-}
+} // class HelloFamily
