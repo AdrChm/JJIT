0a0813fe8fc496dcb641ec04d1d20dbb3ef308cb
AdrChm <adrianchmielewski11@gmail.com>
2023-03-30 13:35:13 +0200

    13.12.1 TimesTable - JScrollPane

--- TimesTable.java
+++ TimesTable.java
@@ -1,17 +1,25 @@
 import java.awt.Container;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 // Program to show a times table for a multiplier chosen by the user.
+// The user also chooses the size of the table.
 public class TimesTable extends JFrame implements ActionListener
 {
 	// A text field for the user to enter the multiplier.
 	private final JTextField multiplierJTextField = new JTextField(5);
+
+	// A text field for the user to enter the size of the table.
+	private final JTextField tableSizeJTextField = new JTextField("10");
 
 	// A text area for the resulting times table, 15 lines or 20 characters.
 	private final JTextArea displayJTextArea = new JTextArea(15,20);
@@ -20,15 +28,28 @@ public class TimesTable extends JFrame implements ActionListener
 	public TimesTable()
 	{
 		setTitle("Times Table");
-		Container content = getContentPane();
-		content.setLayout(new BorderLayout());
+		Container contents = getContentPane();
+		contents.setLayout(new BorderLayout());
 
-		content.add(multiplierJTextField, BorderLayout.NORTH);
-		content.add(displayJTextArea, BorderLayout.CENTER);
-		displayJTextArea.setEnabled(false);
+		// A JPanel the two text fields.
+		// It will be a GridLayout of two times two,
+		// at the top of the JFrame contents.
+		JPanel numbersPanel = new JPanel();
+		contents.add(numbersPanel, BorderLayout.NORTH);
+		numbersPanel.setLayout(new GridLayout(2,0));
 
+		// Add to JLabels, and two JTextFields to the numbersPanel.
+		numbersPanel.add(new JLabel("Multiplier:"));
+		numbersPanel.add(multiplierJTextField);
+		numbersPanel.add(new JLabel("Table size:"));
+		numbersPanel.add(tableSizeJTextField);
+
+		// The result JScrollPane/JTextArea goes in the centre.
+		contents.add(new JScrollPane(displayJTextArea), BorderLayout.CENTER);
+
+		// The JButton goes at the bottom.
 		JButton displayJButton = new JButton("Display");
-		content.add(displayJButton,BorderLayout.SOUTH);
+		contents.add(displayJButton,BorderLayout.SOUTH);
 		displayJButton.addActionListener(this);
 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -43,11 +64,12 @@ public class TimesTable extends JFrame implements ActionListener
 		displayJTextArea.setText("");
 
 		int multiplier = Integer.parseInt(multiplierJTextField.getText());
+		int tableSize = Integer.parseInt(tableSizeJTextField.getText());
 
 		displayJTextArea.append("--------------------------------\n");
 		displayJTextArea.append("| Times table for " + multiplier + "\n");
 		displayJTextArea.append("--------------------------------\n");
-		for (int thisNumber = 1; thisNumber <= 10; thisNumber++)
+		for (int thisNumber = 1; thisNumber <= tableSize; thisNumber++)
 			displayJTextArea.append("| " + thisNumber + " x " + multiplier
 								   + " = " + thisNumber * multiplier + "\n");
 
