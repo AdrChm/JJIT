8353a4cb33f68f2b588e6bea7a27e6ddcd9955be
AdrChm <adrianchmielewski11@gmail.com>
2023-03-31 21:02:58 +0200

    14.3.2 MarkAnalysis - sorting

--- MarkAnalysis.java
+++ MarkAnalysis.java
@@ -16,37 +16,25 @@ public class MarkAnalysis {
 
         if(numberOfMarks > 0) {
 
-            // Processing each mark getting max and min value
-            // as well as calculating sum of marks.
-            int maxMark = 0;
-            int minMark = 0;
+            // Processing each mark and calculating sum of marks.
             int sumOfMarks = 0;
-            double meanMark = 0;
+
 
             int[] marks = new int[numberOfMarks];
             for (int index = 0; index < numberOfMarks; index++)
             {
                 System.out.print("Enter mark # " + (index + 1) + ": ");
                 marks[index] = inputScanner.nextInt();
-                if(index > 0)
-                {
-                    // Updating min and max marks.
-                    if(maxMark < marks[index])
-                        maxMark = marks[index];
-                    else if (minMark > marks[index])
-                        minMark = marks[index];
+                sumOfMarks += marks[index];
 
-                    sumOfMarks += marks[index];
-                } // if
-                else  // Assigning first mark.
-                {
-                    maxMark = marks[index];
-                    minMark = marks[index];
-                    sumOfMarks = marks[index];
-                } // else
             } // for
 
-            meanMark = sumOfMarks/ (double) numberOfMarks;
+            // Getting max and min value
+            mergeSort(marks);
+            int maxMark = marks[marks.length - 1];
+            int minMark = marks[0];
+
+            double meanMark = sumOfMarks/ (double) numberOfMarks;
 
             // Final report.
             System.out.println();
@@ -63,7 +51,63 @@ public class MarkAnalysis {
         else // Wrong number of marks is entered
             System.out.println("Given number of marks is invalid. Data will not be processed");
 
-
-
     } // main
+
+    // For this task I decided to use merge sort as it seems to be one of the most reasonable algorithms.
+    public static void mergeSort(int [] anArray)
+    {
+        // Divides array until there is one element.
+       if(anArray.length < 2)
+            return; // Nothing to be done here continue.
+
+           int[] firstHalfArray = new int[anArray.length /2];
+           int[] secondHalfArray = new int[anArray.length - firstHalfArray.length];
+
+           // Filling the elements for first and then second sub array.
+           for (int index = 0; index < firstHalfArray.length; index++)
+                   firstHalfArray[index] = anArray[index];
+
+            for (int index = 0; index < secondHalfArray.length; index++)
+                secondHalfArray[index] = anArray[index + firstHalfArray.length];
+
+           mergeSort(firstHalfArray);
+           mergeSort(secondHalfArray);
+           merge(firstHalfArray,secondHalfArray, anArray);
+
+    } // mergeSort
+
+    // Private helper method to sort merge sorted arrays.
+    public static void merge(int[] array1, int[] array2 ,int[] mergedArray)
+    {
+        int array1Index = 0, array2Index = 0, mergedArrayIndex = 0;
+        while (array1Index < array1.length && array2Index < array2.length)
+        {
+            if(array1[array1Index] <= array2[array2Index])
+            {
+                mergedArray[mergedArrayIndex] = array1[array1Index];
+                array1Index++;
+            } // if
+            else
+            {
+                mergedArray[mergedArrayIndex] = array2[array2Index];
+                array2Index++;
+            } // else
+            mergedArrayIndex++;
+        } // while
+
+        // Filing remaining part
+        while (array1Index < array1.length){
+            mergedArray[mergedArrayIndex] = array1[array1Index];
+            array1Index++;
+            mergedArrayIndex++;
+        } // while
+
+        while (array2Index < array2.length){
+            mergedArray[mergedArrayIndex] = array2[array2Index];
+            array2Index++;
+            mergedArrayIndex++;
+        } // while
+
+    } // merge
+
 } // class MarkAnalysis
