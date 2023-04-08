d009a7a1db367dd01f25bb0ff197b4eb716cfefd
AdrChm <adrianchmielewski11@gmail.com>
2023-04-08 18:15:31 +0200

    15.4.2 FishTankVolume - try/catch

--- FishTankVolume.java
+++ FishTankVolume.java
@@ -5,16 +5,7 @@ public class FishTankVolume
 {
 	public static void main(String [] args)
 	{
-		boolean inputValidity = true;
-
-		if(args.length <= 2)
-			inputValidity = false;
-		else
-			for(String argument : args)
-				if(!isInteger(argument))
-					inputValidity = false;
-
-		if(inputValidity)
+		try
 		{
 			int width = Integer.parseInt(args[0]);
 			int depth = Integer.parseInt(args[1]);
@@ -24,25 +15,12 @@ public class FishTankVolume
 			System.out.println("The volume of a tank with dimensions "
 					+ "(" + width + "," + depth + "," + height + ") "
 					+ "is " + volume);
-		} // if
-		else
+		} // try
+		catch (Exception exception)
+		{
 			System.out.println("Given input is not a list of proper integers, try again.");
-
+			System.err.println("the problem: " + exception.getCause());
+			System.err.println(exception);
+		} // catch
 	} // main
-
-	// Helper method to validate user input.
-	private static boolean isInteger(String input)
-	{
-		// If any character is not a digit.
-		for (int index = 0; index < input.length(); index++)
-			if(!Character.isDigit(input.charAt(index)))
-				return false;
-
-		// If entered value exceeds integer capacity.
-		if(Long.parseLong(input) > Integer.MAX_VALUE)
-			return false;
-
-		return true;
-	} // isInteger
-
 } // class FishTankVolume
