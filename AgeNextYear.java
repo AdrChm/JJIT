2bbf81c78e99ffb50f10bbc758b7432946bfbaf9
AdrChm <adrianchmielewski11@gmail.com>
2023-04-08 19:06:11 +0200

    15.5.1 AgeNextYear - multiple catch

--- AgeNextYear.java
+++ AgeNextYear.java
@@ -13,12 +13,27 @@ public class AgeNextYear
 			System.out.println("My age now is " + myAgeNow);
 			System.out.println("My age next year will be " + myAgeNextYear);
 		} // try
-		catch(Exception exception)
+		catch(ArrayIndexOutOfBoundsException exception)
 		{
-			System.out.println("Please supply your age, as a whole number.");
+			System.out.println("Please supply your age.");
+			System.err.println(exception);
+		} // catch
+		catch (NumberFormatException exception)
+		{
+			System.out.println("Your age must be a whole number!");
 			System.out.println("Exception message was: '" + exception.getMessage() + "'");
 			System.err.println(exception);
 		} // catch
+
+		// Other exceptions should not happen,
+		// but we catch anything else, lest we have overlooked something.
+		catch (Exception exception)
+		{
+			System.out.println("Something unforeseen has happened.");
+			System.out.println("Exception message was: '" + exception.getMessage() + "'");
+			System.err.println(exception);
+		} // catch
+
 	} // main
 
 } // class AgeNextYear
