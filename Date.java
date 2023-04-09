587da36e0aaeb55540b9191328f123c5eb81eac2
AdrChm <adrianchmielewski11@gmail.com>
2023-04-09 17:39:02 +0200

    15.8.1 Date - exception hanndling

--- Date.java
+++ Date.java
@@ -11,23 +11,34 @@ public class Date
 
 	/**
 	 * Sets the present date.
-	 * This is ignored if the date has already been set.
+	 * The date must not have already been set.
 	 *
 	 * @param requiredPresentDate The required date for the present day.
+	 *
+	 * @throws Exception if present date has already been set or if given
+	 * 					 date is null.
 	 */
-	public static void setPresentDate(Date requiredPresentDate)
+	public static void setPresentDate(Date requiredPresentDate) throws Exception
 	{
-		if (presentDate == null)
-			presentDate = requiredPresentDate;
+		if (requiredPresentDate == null)
+			throw new Exception("Present date cannot be set to null.");
+		if (presentDate != null)
+			throw new Exception("Present date has already been set.");
+
+		presentDate = requiredPresentDate;
 	} // setPresentDate
 
 	/**
 	 * Gets the present date.
 	 *
 	 * @return The present date, or null if it has not been set.
+	 *
+	 * @throws Exception if present date has not been set.
 	 */
-	public static Date getPresentDate()
+	public static Date getPresentDate() throws Exception
 	{
+		if (presentDate == null)
+			throw new Exception("Present date has not been set.");
 		return presentDate;
 	} // getPresentDate
 
@@ -35,35 +46,79 @@ public class Date
 	private final int day, month, year;
 
 	/**
-	 * Constructs a date. If the day and/or month components are zero
-	 * or negative, they are treated as being 1; if they are too
-	 * larege, they are treated as being the largest value allowed.
+	 * Constructs a date, given the three components.
 	 *
 	 * @param requiredDay The required day.
 	 * @param requiredMonth The required month.
 	 * @param requiredYear The required year.
+	 *
+	 * @throws Exception if the date components do not form a legal date since
+	 * 					 the start od 1753 (post Gregorian Reformation).
 	 */
 	// Construct a date -- given the required day, month and year.
 	public Date(int requiredDay, int requiredMonth, int requiredYear)
+			throws Exception
 	{
 		year = requiredYear;
-		if(requiredMonth < 1) 			month = 1;
-		else if (requiredMonth > 12) 	month = 12;
-		else 							month = requiredMonth;
+		month = requiredMonth;
+		day = requiredDay;
 
-		if(requiredDay < 1) 					day = 1;
-		else if (requiredDay > daysInMonth())	day = daysInMonth();
-		else 									day= requiredDay;
+		// Now check these components are legal, throw exception if not.
+		checkDateIsLegal();
 	} // Date
-/**
- * Yields the day component of this date.
- *
- * @return The day of this date.
- */
-public int getDay()
-{
-	return day;
-} // getDay
+
+	private void checkDateIsLegal() throws Exception
+	{
+		if (year < 1753)
+			throw new Exception("Year " + year + " must be >= 1753");
+
+		if (month < 1 ||  month < 12)
+			throw new Exception("Month " + month + " must be from 1 to 12");
+
+		if (day < 1 ||  day > daysInMonth())
+			throw new Exception("Day " + month + " must be from 1 to " + daysInMonth()
+								+ " for " + month + " / " + year);
+
+	} // checkDateIsLegal
+
+	/**
+	 *  Constructs a date, given a String holding the
+	 *  day/month/year representation of the date.
+	 *
+	 * @param dateString The required date as day/month/year.
+	 *
+	 * @throws Exception if dateString is not legal.
+	 */
+	public Date(String dateString) throws Exception
+	{
+		try
+		{
+			String [] dateElements = dateString.split("/");
+			if(dateElements.length > 3)
+				// This exception will be caught below.
+				throw new Exception("Too many date elements");
+
+			day = Integer.parseInt(dateElements[0]);
+			month = Integer.parseInt(dateElements[1]);
+			year = Integer.parseInt(dateElements[2]);
+
+		} // try
+		catch (Exception exception)
+		{
+			throw new Exception("Date '" + dateString + "' is not in day/month/year format", exception);
+		} // catch
+		// If we get to here, we just check if the date components are legal.
+		checkDateIsLegal();
+	} // Date
+	/**
+	 * Yields the day component of this date.
+	 *
+	 * @return The day of this date.
+	 */
+	public int getDay()
+	{
+		return day;
+	} // getDay
 
 	/**
 	 * Yields the month component of this date.
@@ -104,8 +159,10 @@ public int getDay()
 	 * as this one; a value less than 0 if this date is less than the
 	 * other; and a value greater than 0 if this date is greater than
 	 * the other.
+	 *
+	 * @throws NullPointerException if other is null.
 	 */
-	public int compareTo(Date other)
+	public int compareTo(Date other) throws NullPointerException
 	{
 		if (year != other.year) 		return year - other.year;
 		else if (month != other.month) 	return month - other.month;
@@ -118,8 +175,10 @@ public int getDay()
 	 * @param other The other date to compare with.
 	 *
 	 * @return true if and only if they represent the same date.
+	 *
+	 * @throws NullPointerException if other is null.
 	 */
-	public boolean equals(Date other)
+	public boolean equals(Date other) throws NullPointerException
 	{
 		return compareTo(other) == 0;
 	} // equals
@@ -129,8 +188,10 @@ public int getDay()
 	 * @param other The other date to compare with.
 	 *
 	 * @return true if and only if this date is less than the other.
+	 *
+	 * @throws NullPointerException if other is null.
 	 */
-	public boolean lessThan(Date other)
+	public boolean lessThan(Date other) throws NullPointerException
 	{
 		return compareTo(other) < 0;
 	} // lessThan
@@ -140,8 +201,10 @@ public int getDay()
 	 * @param other The other date to compare with.
 	 *
 	 * @return true if and only if this date is greater than the other.
+	 *
+	 * @throws NullPointerException if other is null.
 	 */
-	public boolean greaterThan(Date other)
+	public boolean greaterThan(Date other) throws NullPointerException
 	{
 		return compareTo(other) > 0;
 	} // greaterThan
@@ -166,7 +229,15 @@ public int getDay()
 				newYear++;
 			} // if
 		} // if
-		return new Date(newDay, newMonth, newYear);
+		// This cannot cause an exception, but Java does not know that.
+		try
+		{
+			return new Date(newDay, newMonth, newYear);
+		} // try
+		catch (Exception exception)
+		{
+			return null;
+		} // catch
 	} // addDay
 
 	/**
@@ -178,15 +249,19 @@ public int getDay()
 	 */
 	public Date addMonth()
 	{
-		int newMonth = month;
+		int newDay = day;
+		int newMonth = month + 1;
 		int newYear = year;
 		if(newMonth > 12)
 		{
 			newMonth = 1;
 			newYear++;
 		} // if
-		// Day will be corrected in constructor if too high.
-		return new Date(day, newMonth, newYear);
+		if(newDay > daysInMonth(newMonth, newYear))
+			newDay = daysInMonth(newMonth, newYear);
+		// This may cause an exception, but Java doesn't know that.
+		try { return new Date(newDay, newMonth, newYear); }
+		catch (Exception exception) { return null; }
 	} // addMonth
 
 	/**
@@ -197,64 +272,86 @@ public int getDay()
 	 */
 	public Date addYear()
 	{
-		// Day will be corrected in constructor for 29th February.
-		return new Date(day, month, year + 1);
+		// This cannot cause an exception, but Java does not know that.
+		try
+		{
+			if(month == 2 && day == 29)
+				return new Date(28,month,year + 1);
+			else
+				return new Date(day, month, year + 1);
+		} // try
+		catch (Exception exception) { return null; }
 	} // addYear
 
 	/**
 	 * Constructs a new date which is one day earlier than this one.
+	 * This can throw an exception
+	 * if the new date is earlier than the start of 1753.
 	 *
 	 * @return A new date which is one day earlier than this one.
+	 *
+	 * @throws Exception if the new date is earlier than the start of 1753.
 	 */
-	public Date subtractDay()
+	public Date subtractDay() throws Exception
 	{
 		int newDay = day - 1;
 		int newMonth = month;
 		int newYear = year;
 		if (newDay < 1)
 		{
-			// Will be corrected in constructor if too high.
-			newDay = 31;
 			newMonth--;
 			if(newMonth < 1)
 			{
 				newMonth = 12;
 				newYear--;
 			} // if
+			newDay = daysInMonth(newMonth, newYear);
 		} // if
 		return new Date(newDay, newMonth, newYear);
 	} // subtractDay
 
 	/**
 	 * Constructs a new date which is one month earlier than this one.
+	 * This can throw an exception
+	 * if the new date is earlier than the start of 1753.
 	 * If the day is too large for that month, it is truncated to
 	 * the number of days in that month.
 	 *
 	 * @return A new date which is one month earlier than this one.
+	 *
+	 * @throws Exception if the new date is earlier than the start of 1753.
 	 */
-	public Date subtractMonth()
+	public Date subtractMonth() throws Exception
 	{
-		int newMonth = month;
+		int newDay = day;
+		int newMonth = month - 1;
 		int newYear = year;
 		if(newMonth < 1)
 		{
 			newMonth = 12;
 			newYear--;
 		} // if
-		// Day will be corrected in constructor if too high.
-		return new Date(day, newMonth, newYear);
+		if (newDay > daysInMonth(newMonth, newYear))
+			newDay = daysInMonth(newMonth, newYear);
+		return new Date(newDay, newMonth, newYear);
 	} // subtractMonth
 
 	/**
 	 * Constructs a new date which is one year later than given one.
+	 * This can throw an exception
+	 * if the new date is earlier than the start of 1753.
 	 * If this date is a leap day, it returns 28th February of the next year.
 	 *
 	 * @return A new date which is one year later than this one.
+	 *
+	 * @throws Exception if the new date is earlier than the start of 1753.
 	 */
-	public Date subtractYear()
+	public Date subtractYear() throws Exception
 	{
-		// Day will be corrected in constructor for 29th February.
-		return new Date(day, month, year - 1);
+		if (day == 29 && month == 2)
+			return new Date(28, month, year - 1);
+		else
+			return new Date(day, month, year - 1);
 	} // subtractYear
 
 	/**
@@ -266,8 +363,10 @@ public int getDay()
 	 * @param other The other date.
 	 *
 	 * @return The distance in days.
+	 *
+	 * @throws NullPointerException if the other is null.
 	 */
-	public int daysFrom(Date other)
+	public int daysFrom(Date other) throws NullPointerException
 	{
 		// The code here is a prototype
 		// -- the result should be computed more efficiently than this!
@@ -284,47 +383,54 @@ public int getDay()
 			return noOfDaysDistance;
 		} // else if
 		else
-		{
-			Date someDate = subtractDay();
-			int noOfDaysDistance = -1;
-			while(someDate.greaterThan(other))
+			try // We should not get an exception form subtractDay,
+				// because target date is legal. But Java does not know this.
 			{
-				someDate = someDate.subtractDay();
-				noOfDaysDistance--;
-			} // while
-			return noOfDaysDistance;
-		} // else
+				Date someDate = subtractDay();
+				int noOfDaysDistance = -1;
+				while(someDate.greaterThan(other))
+				{
+					someDate = someDate.subtractDay();
+					noOfDaysDistance--;
+				} // while
+				return noOfDaysDistance;
+			} // try
+			// java does not know we cannot get an exception.
+			catch (Exception e) { return 0; }
 	} // daysFrom
 
 	// Calculate the number of days in the month.
 	private int daysInMonth()
 	{
-		switch(month)
-		{
-			case 1: return 31;
-			case 2: if (isLeapYear()) return 29;
-					else 			  return 28;
-			case 3: return 31;
-			case 4: return 30;
-			case 5: return 31;
-			case 6: return 30;
-			case 7: return 31;
-			case 8: return 31;
-			case 9: return 30;
-			case 10: return 31;
-			case 11: return 30;
-			case 12: return 31;
-			default: return 0;
-		} // switch
+		return daysInMonth(month, year);
 	} // daysInMonth
+
+	// Number of days in each month for normal and leap years.
+	// The first index (0) is not used.
+	private static final int[]
+		DAYS_PER_MONTH_NON_LEAP_YEAR
+			// Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
+		 = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
+		DAYS_PER_MONTH_LEAP_YEAR
+		 = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+	// Calculate the number of days in a given month for a given year.
+	// This will never be called with a month out of range 1 to 12.
+	private static int daysInMonth(int month, int year)
+	{
+		if(isLeapYear(year))
+			return DAYS_PER_MONTH_LEAP_YEAR[month];
+		else
+			return DAYS_PER_MONTH_NON_LEAP_YEAR[month];
+	} // daysInMonth
+
 	// Return true if and only if year is leap year.
 	// (ignoring the pre Gregorian Reformation complication -- for now.)
 	// Year is a leap year if it is divisible by 4
 	// 						and is not divisible by 100
 	// 							or is divisible by 400.
-	private boolean isLeapYear()
+	private static boolean isLeapYear(int year)
 	{
 		return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 	} // isLeapYear
 } // class Date
-
