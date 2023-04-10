b0efb74462e5c2ef8978c1aaf6595d746265e370
AdrChm <adrianchmielewski11@gmail.com>
2023-04-10 18:12:47 +0200

    16.8.4 TestPersonSubclasses - MoodyPerson

--- NationalLottery/src/TestPersonSubclasses.java
+++ NationalLottery/src/TestPersonSubclasses.java
@@ -8,21 +8,39 @@ public class TestPersonSubclasses {
                 new AudienceMember("Ivana Di Yowt"),
                 new Director("Sir Lance Earl Otto"),
                 new Psychic("Miss T. Peg de Gowt"),
-                new Punter("Ian Arushfa Rishy Ving"),
+                new Punter("Ian Arushfa Rishly Ving"),
+                new Teenager("Homer Nalzone"),
                 new TVHost("Terry Bill Woah B'Gorne")
             };
         for(Person person : persons)
             testPerson(person);
 
+
     } // main
 
     // Make the given person speak, reporting the before and after toString.
-    public static void testPerson(Person person)
+    private static void testPerson(Person person)
     {
         System.out.println("-------------------------------------------------");
         System.out.println(person);
         person.speak();
         System.out.println(person);
+
+        if( person instanceof MoodyPerson)
+            testMoodyPerson((MoodyPerson) person);
+
     } // testPerson
+
+    // Make the given moody person change happiness then speak,
+    // reporting the after toString; all twice.
+    private static void testMoodyPerson(MoodyPerson moodyPerson)
+    {
+        for (int count = 1; count <= 2; count++)
+        {
+            moodyPerson.setHappy(!moodyPerson.isHappy());
+            moodyPerson.speak();
+            System.out.println(moodyPerson);
+        } // for
+    } // testMoodyPerson
 
 } // class TestPersonSubclasses
