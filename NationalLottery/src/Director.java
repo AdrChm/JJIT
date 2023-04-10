7317035e0e4cdfc410a2ba3612aaa99a37e47e1f
AdrChm <adrianchmielewski11@gmail.com>
2023-04-10 15:39:36 +0200

    16.7.1 Director class

---
+++ NationalLottery/src/Director.java
@@ -0,0 +1,21 @@
+// Representation of a director of the lottery company.
+public class Director extends Person
+{
+    // Constructor is given the person's name.
+    public Director(String name)
+    {
+        super(name);
+    } // Director
+
+    // Returns the name of the type of Person.
+    public String getPersonType()
+    {
+        return "Director";
+    } // getPersonType
+
+    // Returns the Person's current saying.
+    public String getCurrentSaying()
+    {
+        return "Tis business is MY pleasure";
+    } // getCurrentSaying
+} // class Director
