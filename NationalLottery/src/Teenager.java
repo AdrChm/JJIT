5e4763c86e1771cfd36fd6caa35453f9f3137e59
AdrChm <adrianchmielewski11@gmail.com>
2023-04-10 17:15:07 +0200

    16.8.2 Teenager class

---
+++ NationalLottery/src/Teenager.java
@@ -0,0 +1,25 @@
+// Representation of a teenager.
+public class Teenager extends MoodyPerson
+{
+    // The state of the person's happiness.
+    public Teenager(String name)
+    {
+        super(name, false);
+    } // Teenager
+
+    // Returns the name of the type of Person.
+    public String getPersonType()
+    {
+        return "Teenager";
+    } // getPersonType
+
+    // Returns the Person's current saying.
+    public String getCurrentSaying()
+    {
+        if(isHappy())
+            return "Isn't life wonderful?";
+        else
+            return "It's not fair!";
+    } // getCurrentSaying
+
+} // class Teenager
