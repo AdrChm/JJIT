3635c15dfd4d30d8744b46712f437fd00cf49c32
AdrChm <adrianchmielewski11@gmail.com>
2023-04-19 02:15:46 +0200

    16.13.4 tests update

--- TestShoppingBasket.java
+++ TestShoppingBasket.java
@@ -1,17 +1,22 @@
+import java.awt.*;
+
 // Testing class for StockItemPurchaseRequest and ShoppingBasket.
 public class TestShoppingBasket
 {
     public static void main(String[] args)
     {
+        Vendor publisher = new Vendor("O'Reilly Media Inc","04-2789763","1005 Gravenstein Highway N\n Sebastopol\n CA\n United States\n 95472");
+        Vendor hardware = new Vendor("HP Inc.", "94-1081436","1501 Page Mill Road\n Palo Alto\n California\n 94304");
+
         StockItem [] stockItems =
                 {
-                    /* 0 */ new MouseMat("Plain Blue cloth, foam backed", 150, 10),
-                    /* 1 */ new MouseMat("Pink vinyl with fluffy trim", 350, 10),
-                    /* 2 */ new Book("List of all items and prices", 150, 10),
-                    /* 3 */ new Book("Build a gaming monster", 1799, 0),
-                    /* 4 */ new CPU(1500, 10),
-                    /* 5 */ new HardDisc(5500, 10),
-                    /* 6 */ new Keyboard(200, 10),
+                    /* 0 */ new MouseMat("Plain Blue cloth, foam backed", 150, 10, hardware),
+                    /* 1 */ new MouseMat("Pink vinyl with fluffy trim", 350, 10, hardware),
+                    /* 2 */ new Book("List of all items and prices", 150, 10, new Vendor("Our store", "01-1000100", "here")),
+                    /* 3 */ new Book("Build a gaming monster", 1799, 0, publisher),
+                    /* 4 */ new CPU(1500, 10, 2.4, "EPYC", hardware),
+                    /* 5 */ new HardDisc(5500, 10, 500, 3.5, 7200, 250, hardware),
+                    /* 6 */ new Keyboard(200, 10,  new Color(1,1,1), 105, "small display monitor", hardware)
                 };
 
         System.out.println("Stock before purchase:");
--- TestStockItemSubclasses.java
+++ TestStockItemSubclasses.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 // Program to test StockItem subclasses.
 // I hope there will be changes in StockItem in upcoming chapters.
 public class TestStockItemSubclasses
@@ -5,16 +7,18 @@ public class TestStockItemSubclasses
     // Creating each subclass of the StockItem.
     public static void main(String[] args)
     {
+        Vendor publisher = new Vendor("O'Reilly Media Inc.","04-2789763","1005 Gravenstein Highway N\n Sebastopol\n CA\n United States\n 95472");
+        Vendor hardware = new Vendor("HP Inc.", "94-1081436","1501 Page Mill Road\n Palo Alto\n California\n 94304");
         StockItem [] inventory =
         {
-                new MouseMat("Plain blue cloth, foam backed", 1000, 20),
-                new MouseMat("Gaming mouse mat with led lightning", 4000, 20),
-                new Book("List of all items and prices", 0, 100),
-                new Book("Java: Just in Time", 15000, 100),
-                new Keyboard( 4999, 15),
-                new CPU( 99999, 10),
-                new HardDisc( 19999, 20, 500),
-                new ReliableHardDisc( 19999, 20, 500, 10)
+                new MouseMat("Plain blue cloth, foam backed", 1000, 20, hardware),
+                new MouseMat("Gaming mouse mat with led lightning", 4000, 20, hardware),
+                new Book("List of all items and prices", 0, 100, new Vendor("Our store.", "01-1000100", "here")),
+                new Book("Java: Just in Time", 15000, 100, publisher),
+                new Keyboard( 4999, 15, new Color(1,1,1), 105, "small display monitor", hardware),
+                new CPU( 99999, 10, 2.4, "EPYC", hardware),
+                new HardDisc( 19999, 20, 500, 3.5, 7200, 250, hardware),
+                new ReliableHardDisc( 19999, 20, 500, 10,3.5, 7200, 250, hardware)
         };
 
             for(StockItem item: inventory)
@@ -46,13 +50,12 @@ public class TestStockItemSubclasses
         item.setPriceExVat((int) Math.round(item.getPriceExVat() * 0.8));
         System.out.println(item);
 
-        if(item instanceof TextDescriptionStockItem)
-            testTextDescriptionStockItem((TextDescriptionStockItem) item);
+            testTextDescriptionStockItem(item);
 
     } // test
 
     // Display description of stock item as it changes along the way.
-    private static void testTextDescriptionStockItem(TextDescriptionStockItem item)
+    private static void testTextDescriptionStockItem(StockItem item)
     {
         item.setDescription("Description updated!");
         System.out.println(item);
