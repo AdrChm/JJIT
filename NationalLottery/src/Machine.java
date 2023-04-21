4cb509b02eaf96ad082ba78bd3a31aa3fe0d894f
AdrChm <adrianchmielewski11@gmail.com>
2023-04-21 23:44:49 +0200

    17.4.4 Machine - exception handling

--- NationalLottery/src/Machine.java
+++ NationalLottery/src/Machine.java
@@ -3,9 +3,11 @@
 public class Machine extends BallContainer
 {
     // Conductor is given the name and size.
-    public Machine(String name, int size)
+    public Machine(String name, int size) throws BallContainerException
     {
         super(name, size);
+        if (size < 2)
+            throw new MachineException("Size must be at least 2");
     } // Machine
 
     @Override
@@ -17,12 +19,10 @@ public class Machine extends BallContainer
 
     // Randomly choose a ball in the game, and ejects it.
     // The ejected ball is returned. If the machine is empty then
-    // it has no effect, and returns null.
-    public Ball ejectBall()
+    // it throws an exception.
+    public Ball ejectBall() throws MachineException
     {
-        if (getNoOfBalls() <= 0)
-            return null;
-        else
+        try
         {
             // Math.random() * getNoOfBalls yields a number
             // which is >= 0 and < number of balls.
@@ -34,7 +34,11 @@ public class Machine extends BallContainer
             removeBall();
 
             return ejectedBall;
-        } // else
+        } // try
+        catch (BallContainerException exception)
+        {
+            throw new MachineException("Cannot eject ball: is empty", exception);
+        } // catch
     } // ejectBall
 
 } // class Machine
