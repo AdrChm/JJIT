6dc1605be1a1d9c169f42c483ccdebe5939447dc
AdrChm <adrianchmielewski11@gmail.com>
2023-04-21 17:37:09 +0200

    17.3.5 DateDifferenceUnforeseenTest class

---
+++ Chapter17/src/DateDifferenceUnforeseenTest.java
@@ -0,0 +1,9 @@
+// Test DateDifference with a null argument array.
+public class DateDifferenceUnforeseenTest
+{
+    public static void main(String[] args)
+    {
+        DateDifference.main(null);
+    } // main
+
+} //  class DateDifferenceUnforeseenTest
