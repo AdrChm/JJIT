5607f597989cf2f093a130cf9dbf8e9fa14335fa
AdrChm <adrianchmielewski11@gmail.com>
2023-04-25 16:30:03 +0200

    18.5.1 LineNumber - output file

--- LineNumber.java
+++ LineNumber.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-// Program to add a line number to the lines from the standard input
-// and show the result on the standard output.
+// Program to add a line number to the lines from an input file
+// and produce the result in an output file.
+// The two file names are given as command line arguments.
 public class LineNumber
 {
     // The minimum number of digits in a line number.
@@ -12,30 +15,51 @@ public class LineNumber
     // The format to use with printf for the line number and line.
     private static final String LINE_FORMAT
            = "%0" + MINIMUM_LINE_NUMBER_DIGITS + "d %s%n";
+
     // Read each line from input, and copy to the output with a count.
     public static void main(String[] args)
     {
-        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader input = null;
+        PrintWriter output = null;
         try
         {
+            if(args.length != 2)
+                throw new LineNumberException
+                        ("There must be exactly two arguments: infile outfile");
+
+            input = new BufferedReader(new FileReader(args[0]));
+            output = new PrintWriter(new FileWriter(args[1]));
+
             // Now copy input to output, adding line numbers.
             int noOfLinesReadSoFar = 0;
             String currentLine;
             while((currentLine = input.readLine()) != null)
             {
                 noOfLinesReadSoFar++;
-                System.out.printf(LINE_FORMAT, noOfLinesReadSoFar, currentLine);
+                output.printf(LINE_FORMAT, noOfLinesReadSoFar, currentLine);
             } // while
         } // try
+        catch (LineNumberException exception)
+        {
+            // We report LineNumberExceptions to standard output.
+            System.out.println(exception.getMessage());
+        } // catch
         catch (IOException exception)
         {
-            System.err.println(exception);
+            // Other exceptions go to standard error.
+            System.out.println(exception);
         } // catch
         finally
         {
-            try{ input.close(); }
+            try { if(input != null) input.close(); }
             catch (IOException exception)
-            { System.err.println("Could not close the input"); }
+                { System.err.println("Could not close the input " + exception); }
+            if(output != null)
+            {
+                output.close();
+                if(output.checkError())
+                    System.err.println("Something went wrong with the output");
+            } // if
         } // finally
     } // main
 
