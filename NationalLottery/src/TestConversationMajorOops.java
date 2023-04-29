17b6e7559c9eaf53ac8d5f13d477cff54f6b9b65
AdrChm <adrianchmielewski11@gmail.com>
2023-04-30 00:14:20 +0200

    19.7.2 TestConversationMajorOops class -  outside the raw type

---
+++ NationalLottery/src/TestConversationMajorOops.java
@@ -0,0 +1,23 @@
+// Create conversation of persons and make them speak.
+public class TestConversationMajorOops
+{
+
+    public static void main(String[] args)
+    {
+        // A conversation of AudienceMembers.
+        Conversation moodyChat = new Conversation();
+
+        moodyChat.addPerson("TA 1");
+        moodyChat.addPerson(new CleverPunter("CP 1"));
+        moodyChat.addPerson(new Worker("W 1"));
+        moodyChat.addPerson(new TraineeWorker("TW 1", 0.5));
+        System.out.printf("%s%n%n", moodyChat);
+        for (int count = 1; count <= moodyChat.getSize() ; count++)
+        {
+            moodyChat.speak();
+            System.out.printf("%s%n%n", moodyChat);
+        } // for
+
+    } // main
+
+} // class TestConversationMajorOops
