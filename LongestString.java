a169219b25267dd9a636f813af3c47ed770f986d
AdrChm <adrianchmielewski11@gmail.com>
2023-04-29 15:34:49 +0200

    19.2.1 LongestString class

---
+++ LongestString.java
@@ -0,0 +1,25 @@
+// Contains a method to find the position of the longest string in an array.
+public class LongestString
+{
+    // Find the longest string in the given array.
+    // Return a pair containing it and its position.
+    // Throw IllegalArgumentException if array is null or empty.
+    public static Pair findLongestString(String [] array) throws IllegalArgumentException
+    {
+        if(array == null || array.length == 0)
+            throw new IllegalArgumentException("Array must exist and be non-empty");
+
+        String longestString = array[0];
+        int longestIndex = 0;
+        for (int index = 1; index < array.length; index++)
+        {
+            if(longestString.length() < array[index].length())
+            {
+                longestString = array[index];
+                longestIndex = index;
+            } // if
+        }
+        return new Pair(longestString, new Integer(longestIndex));
+    } // findLongestString
+
+} // class LongestString
