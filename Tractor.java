ab085581c15d636fe2a22885aeaa8a4463c837c1
AdrChm <adrianchmielewski11@gmail.com>
2023-04-30 14:49:41 +0200

    20.2.2 Vehicle, Tractor, Car class

---
+++ Tractor.java
@@ -0,0 +1,7 @@
+// Representation of a tractor.
+public class Tractor extends Vehicle
+{
+
+    // ... Loss of stuff specific to a tractor.
+
+} // class Tractor
---
+++ Vehicle.java
@@ -0,0 +1,7 @@
+// Representation of an abstract vehicle.
+public class Vehicle
+{
+
+    // ... Loss of stuff about vehicles in general.
+
+} // class Vehicle
