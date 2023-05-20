c25190c587deef704e1ac10ec952ab3e01403139
AdrChm <adrianchmielewski11@gmail.com>
2023-05-20 20:48:58 +0200

    21.8.1 WordWithFrequency - compareTo, equals

--- WordWithFrequency.java
+++ WordWithFrequency.java
@@ -1,5 +1,5 @@
 // A pairing of a word with its frequency so far.
-public class WordWithFrequency
+public class WordWithFrequency implements Comparable<WordWithFrequency>
 {
     // The word, occurrences of which are being conducted.
     private final String word;
@@ -26,5 +26,33 @@ public class WordWithFrequency
     {
         return word + " " + frequencySoFar;
     } // toString
+
+    // Compare this with the given other, returning negative, zero or positive.
+    // Order first on descending frequency, then on ascending word.
+    @Override
+    public int compareTo(WordWithFrequency other)
+    {
+        if(frequencySoFar != other.frequencySoFar)
+            return other.frequencySoFar - frequencySoFar;
+        else
+            return word.compareTo(other.word);
+    } // compareTo
+
+    // Return true if and only if the given object is equivalent to this one.
+    @Override
+    public boolean equals(Object other)
+    {
+        if(other instanceof WordWithFrequency)
+            return compareTo((WordWithFrequency) other) == 0;
+        else
+            return super.equals(other);
+    } // equals
+
+    // A hash code for this object: equivalent one has the same hash code.
+    @Override
+    public int hashCode()
+    {
+        return frequencySoFar * 31 + word.hashCode() * 37;
+    } // hashCode
 
 } // class WordWithFrequency
